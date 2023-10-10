5aa5224be1588c59a98f0d76a01c9b7875dc3b70
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-10-11 01:36:50 +0200

    init commit

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,13 +1,31 @@
 package com.example.bookstore;
 
+import com.example.bookstore.model.Book;
+import com.example.bookstore.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+
+            Book fellowship = new Book("The Fellowship of the Ring",
+                    "J.R.R. Tolkien", "9788845292248", BigDecimal.valueOf(1999));
+            bookService.save(fellowship);
+            System.out.println(bookService.findAll());
+        };
+    }
 }

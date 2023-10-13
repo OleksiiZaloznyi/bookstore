ca033a08aa9e6de006182146e42fe3eb1abc36c0
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-10-13 17:08:16 +0200

    last update

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -1,13 +1,14 @@
 package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository {
-    Book save(Book book);
-
-    Optional<Book> findById(Long id);
-
-    List<Book> findAll();
+public interface BookRepository extends JpaRepository<Book, Long> {
+//    Book save(Book book);
+//
+//    Optional<Book> findById(Long id);
+//
+//    List<Book> findAll();
 }

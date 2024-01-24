4a07d51b2b49a6395971fc4d0d4b47c5283bbb9d
Oleksii Zaloznyi <118694500+OleksiiZaloznyi@users.noreply.github.com>
2024-01-25 00:00:42 +0200

    Revert "Spring Boot JWT completed"

--- src/main/java/com/example/bookstore/dto/category/CategoryRequestDto.java
+++
@@ -1,9 +0,0 @@
-//package com.example.bookstore.dto.category;
-//
-//import lombok.Data;
-//
-//@Data
-//public class CategoryRequestDto {
-//    private String name;
-//    private String description;
-//}
--- src/main/java/com/example/bookstore/mapper/BookMapper.java
+++ src/main/java/com/example/bookstore/mapper/BookMapper.java
@@ -2,35 +2,16 @@ package com.example.bookstore.mapper;
 
 import com.example.bookstore.config.MapperConfig;
 import com.example.bookstore.dto.book.BookDto;
-import com.example.bookstore.dto.book.BookDtoWithoutCategoryIds;
 import com.example.bookstore.dto.book.CreateBookRequestDto;
 import com.example.bookstore.dto.book.UpdateBookRequestDto;
 import com.example.bookstore.model.Book;
-import com.example.bookstore.model.Category;
-import java.util.Set;
-import java.util.stream.Collectors;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
-    Book toEntity(CreateBookRequestDto requestDto);
+    Book toModel(CreateBookRequestDto requestDto);
 
     Book toModelUpdate(UpdateBookRequestDto requestDto);
-
-    BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
-
-    @AfterMapping
-    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
-        if (bookDto != null && book != null) {
-            Set<Long> categoryIds = book.getCategories().stream()
-                    .map(Category::getId)
-                    .collect(Collectors.toSet());
-
-            bookDto.setCategoryIds(categoryIds);
-        }
-    }
 }
